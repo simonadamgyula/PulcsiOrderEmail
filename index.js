165a23e671c472a675fdee3aad0443c287c59f71
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2024-09-23 18:58:22 +0200

    im smart

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 var http = require('http');
 const sendEmail = require('./email.js');
 
-const person = {
-    id: "",
-    name: "Kiss Pista",
-    email: "jedlikpulcsi@gmail.com",
-    class: "10.A",
-    phone: "06301234567"
-}
-
-const orders = [
-    {
-        imageId: "red_gepesz_back",
-        product: "Gépész Pulcsi",
-        size: "M",
-        color: "Piros",
-        price: 6000,
-        quantity: 2
-    },
-    {
-        imageId: "blue_info_back",
-        product: "Infós Pulcsi",
-        size: "S",
-        color: "Kék",
-        price: 6000,
-        quantity: 1
-    }
-]
-
 console.log(process.env.PORT);
 
 http.createServer(function (req, res) {
